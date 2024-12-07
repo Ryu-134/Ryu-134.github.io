306c0940e4a7d234b2b695ee51a48b2b34ca7910
Ryu-134 <caseydane134@gmail.com>
2024-12-06 23:06:03 -0800

    automate deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
-
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter({
-            fallback: '404.html', // Serve a fallback page
-        }),
+        adapter: vercel(), // Use the Vercel adapter
         paths: {
             base: '', // No base path since it's served from the root
         },
     },
+    preprocess: vitePreprocess(), // Add preprocessors here
 };
 
 export default config;
